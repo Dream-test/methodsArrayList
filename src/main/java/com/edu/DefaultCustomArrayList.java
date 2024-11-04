73b2d974dc9ae18fb8c6fd171a8f40fdd691d018
Dream-test <stsiapkou@gmail.com>
2024-11-04 18:46:03 +0100

    Method add element _1

--- src/main/java/com/edu/DefaultCustomArrayList.java
+++ src/main/java/com/edu/DefaultCustomArrayList.java
@@ -1,15 +1,49 @@
 package com.edu;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class DefaultCustomArrayList<E> implements CustomArrayList<E> {
+    private static final int DEFAULT_VOLUME = 10;
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+    static Object elementsData[];
+    static int countData;
 
     public DefaultCustomArrayList() {
+       elementsData = new Object[DEFAULT_VOLUME];
+       countData = 0;
     }
 
     @Override
     public boolean add(E element) {
+        int minVolume = countData + 1;
+        checkAvailableVolume(minVolume);
+        elementsData[countData] = element;
+
         return true;
+    }
+
+    private void checkAvailableVolume(int minVolume) {
+        if (minVolume - elementsData.length > 0) {
+            grow(minVolume);
+        }
+    }
+
+    private void grow(int minVolume) {
+        int oldVolume = elementsData.length;
+        int newVolume = oldVolume +(oldVolume >> 1);
+        if  (newVolume - MAX_ARRAY_SIZE > 0) {
+            newVolume = maxVolume(minVolume);
+        }
+
+        elementsData = Arrays.copyOf(elementsData, newVolume);
+    }
+
+    private int maxVolume(int minVolume) {
+        if ( minVolume < 0) {
+            throw new OutOfMemoryError("Limit arrays elements");
+        }
+        return (minVolume > MAX_ARRAY_SIZE) ? Integer.MAX_VALUE : MAX_ARRAY_SIZE;
     }
 
     @Override
